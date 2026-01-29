54daf64ba4cd05c64a2a40e7e91390a633e8c347
Dikshan Dangol <dikshandangol@MITIRIS-MAC-BOOK.local>
2026-01-29 09:27:44 +0545

    updated images new1

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,25 +3,25 @@ const menu = [
     name: "Satvik Thali",
     description: "A healthy traditional meal.",
     price: "Rs 350",
-    image: "/public/images/satvik.png",
+    image: "/src/assets/satvik.png",
   },
   {
     name: "Seasonal Thali",
     description: "Fresh seasonal vegetables served with rice and lentils.",
     price: "Rs 300",
-    image: "/public/images/seasonal.png",
+    image: "/src/assets/seasonal.png",
   },
   {
     name: "Tulsi Tea",
     description: "Herbal tea made from fresh tulsi leaves.",
     price: "Rs 80",
-    image: "/public/images/tulsi.png",
+    image: "src//assets/tulsi.png",
   },
   {
     name: "amala",
     description: "Fresh amla juice rich in vitamin C.",
     price: "Rs 120",
-    image: "/public/images/amala.png",
+    image: "/src/assets/amala.png",
   },
 ];
 
